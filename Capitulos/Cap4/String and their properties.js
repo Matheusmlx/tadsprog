9500a8380d875b7cf45e65b81aad601b110112e2
Matheusmlx <matheusluiz0303@outlook.com>
2019-03-15 09:43:39 -0300

    String and Their properties

---
+++ Capitulos/Cap4/String and their properties.js
@@ -0,0 +1,18 @@
+//try add a new property
+//values imumutable cannot be changed
+let kim='Kim';
+kim.age=88;
+console.log(kim.age);
+
+console.log("coconuts".slice(4,7));
+
+console.log('coconut'.indexOf("u"));
+
+console.log("one two three".indexOf("ee"));
+
+//trim metodo de corte(retirar os espaço em brancos)
+console.log(" okay   \n ");
+console.log(" okay   \n ".trim());
+
+//zeroPad 
+console.log(String(6).padStart(3,"0"));
