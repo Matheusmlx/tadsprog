c8027fdc149d0ef71c7de6c51115b2ae19b6cc7b
Matheusmlx <matheusluiz0303@outlook.com>
2019-03-17 11:08:53 -0300

    Top1

---
+++ Capitulos/Cap5/Higher-Order Functions.js
@@ -0,0 +1,11 @@
+let total=0,count=1;
+while(count<=10){
+    console.log(total);
+    total+=count;
+    count+=1;
+
+}
+//console.log(total);
+//The second relies on two external functions and is one line long.
+console.log(sum(range(1, 10)));
+
