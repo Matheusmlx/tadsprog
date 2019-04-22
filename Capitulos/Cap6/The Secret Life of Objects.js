a3da9c0ea88c4473771e5c51c49c40f30fb2565a
Matheusmlx <matheusluiz0303@outlook.com>
2019-04-22 10:39:59 -0500

    Methods e Methods Call

--- Capitulos/Cap6/The Secret Life of Objects.js
+++ Capitulos/Cap6/The Secret Life of Objects.js
@@ -1,6 +1,15 @@
 //Methods  - Propriedades que contêm valores de função 
+
 //simple method
-/*
+
+let pessoa = {};
+
+pessoa.estuda=(nome,materia)=>console.log(`${nome} esta estudando ${materia}`);
+
+pessoa.estuda("Matheus","JavaScript");
+
+console.log('-------------------------Methods simples---------------------------')
+
  let rabbit = {};
 
   rabbit.eating=(food)=>{console.log(`The rabbit eating ${food}`)}
@@ -11,29 +20,35 @@
 
  rabbit.speak("I'm alive");
  rabbit.eating("pizza");
- */
 
+
+
+
+
+console.log('-------------------------METHODS---------------------------')
  /*Quando uma função é colocada num objeto, ela se torna um metodo e esta sendo referenciada pelo
  objeto que a chama.. */
+ function estuda(materia){
+     console.log(`${this.nome} esta estudando ${materia}`)
+ }
 
- function speak(line) {
-    console.log(`The ${this.type} coelho says '${line}'`);
-    }
- function hi(){console.log(`Ola ${this.type}`)}
+ pessoa1= {nome:"Matheus",estuda};
+ pessoa2= {nome:"Luiz"};
+pessoa1.estuda("Matematica")
 
-    let whiteRabbit = {type: "white", speak,hi};
-    let hungryRabbit = {type: "hungry", speak};
-
-    //whiteRabbit.speak("Oh my ears and whiskers, " +"how late it's getting!");
-    
+console.log('--------FUNÇÃO CALL()-----------------')
     //Com o call()método, você pode escrever um método que pode ser usado em objetos diferentes.
-    whiteRabbit.hi.call(hungryRabbit);
+    //Ela mei oque chama outro objeto para ser referenciado
+    
+    //whiteRabbit.hi.call(hungryRabbit);
     //speak.call(hungryRabbit, "Burp!");
 
-    console.log('--------ArrowFunctions And Functions-----------------')
-    
-    function normalize(){
-        console.log(this.coords.map(n=>n/this.length));
-    }
+   //Tão pode ser usado com uma função , recendo como parametro  um objeto e o parametro da função
+   estuda.call(pessoa1,"Matematica")
+   pessoa1.estuda.call(pessoa2,"Matematica")
 
-    normalize.call({coords:[0,2,3],length:5})
+console.log('--------CLASSES-----------------')
+
+
+
+   
